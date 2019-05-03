8f24b9fe209f11b4745a99399767c67d23309746
crusiris <cruzsirys@gmail.com>
2019-05-03 02:54:32 +0200

    Solucionando errores de estilo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 
+import { FormsModule } from "@angular/forms";
+
 import { AngularFireModule } from "@angular/fire";
 
 import { AngularFirestoreModule } from "@angular/fire/firestore";
@@ -11,10 +13,14 @@ import { AngularFireAuthModule } from "@angular/fire/auth";
 
 import { environment } from "../environments/environment";
 
+//Componentes
+import { ChatComponent } from "./components/chat/chat.component";
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChatComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule
